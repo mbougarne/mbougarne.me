3fffedafe2c564b945c0d53d1f84199c71d85cfc
Mourad Bougarne <contact@mbougarne.me>
2021-03-07 22:49:02 +0100

    تغيير صلاحيات المستخدم

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Header from "./header"
+import Header from "./Header"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
